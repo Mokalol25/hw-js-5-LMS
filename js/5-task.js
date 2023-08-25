9e30827087a9967cfd686a0200f15c071405a303
Mokalol25 <93078882+Mokalol25@users.noreply.github.com>
2023-08-25 10:28:17 -0400

    d

--- js/5-task.js
+++ js/5-task.js
@@ -1,5 +1,5 @@
 const input = document.querySelector("#font-size-control");
 const span = document.querySelector("#text");
 input.addEventListener("input", (event) => {
-    span.style.fontSize = input.value
+    span.style.fontSize = input.value + "px"
 })
